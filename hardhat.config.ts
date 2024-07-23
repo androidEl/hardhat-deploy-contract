e375bf427506fe747a5f877e247fb1ee65f24589
Elham Yudhistira <bugelindah22@gmail.com>
2024-07-23 15:09:08 +0000

    task 1

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -9,7 +9,7 @@ const config: HardhatUserConfig = {
   networks: {
     swisstronik: {
       url: 'https://json-rpc.testnet.swisstronik.com/', //URL of the RPC node for Swisstronik.
-      accounts: [`0x${process.env.PRIVATE_KEY}`], //Your private key starting with "0x"
+      accounts: [`0xb93832a4f2119373f2eb43e8e748500511cef74330fe15c60e2f75d0ecae7fca`], //Your private key starting with "0x"
       //Make sure you have enough funds in this wallet to deploy the smart contract
     },
   },
--- utils/deployed-address.ts
+++ utils/deployed-address.ts
@@ -1,3 +1,3 @@
-const deployedAddress = ''
+const deployedAddress = '0xFAD70Da226Fa640057693c858FF2ccB6b4c068B3'
 
 export default deployedAddress
